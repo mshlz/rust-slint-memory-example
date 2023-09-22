3d7e5c1b368e41fdd07c80708462ef485c74cb76
mshlz <mateusholzschuh@hotmail.com>
2023-09-22 18:16:49 -0300

    chore: duplicate tiles and shuffle

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,23 @@
 fn main() {
-    MainWindow::new().unwrap().run().unwrap();
+    use slint::Model;
+
+    let main_window = MainWindow::new().unwrap();
+
+    // Fetch the tiles from the model
+    let mut tiles: Vec<TileData> = main_window.get_memory_tiles().iter().collect();
+    // Duplicate them to ensure that we have pairs
+    tiles.extend(tiles.clone());
+
+    // Randomly mix the tiles
+    use rand::seq::SliceRandom;
+    let mut rng = rand::thread_rng();
+    tiles.shuffle(&mut rng);
+
+    // Assign the shuffled Vec to the model property
+    let tiles_model = std::rc::Rc::new(slint::VecModel::from(tiles));
+    main_window.set_memory_tiles(tiles_model.into());
+
+    main_window.run().unwrap();
 }
 
 slint::slint! {
